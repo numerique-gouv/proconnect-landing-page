c6112cc053bfc966ac176788767d792b2b2f6ed2
Douglas DUTEIL <douglasduteil@gmail.com>
2024-07-19 15:21:11 +0200

    fix(stats): remove react-iframe

--- src/Pages/Stats.tsx
+++ src/Pages/Stats.tsx
@@ -1,15 +1,15 @@
-import Iframe from 'react-iframe';
+//
 
 function Stats() {
     return (
         <div className="fr-container">
             <div className="fr-grid-row">
-                <Iframe
+                <iframe
                     className="fr-col-12"
-                    width="100%"
                     height="2000"
-                    url="https://stats.agentconnect.gouv.fr/public/dashboard/59405cec-eb60-47c6-9eb2-0f4ee9a0378c"
-                ></Iframe>
+                    src="https://stats.agentconnect.gouv.fr/public/dashboard/59405cec-eb60-47c6-9eb2-0f4ee9a0378c"
+                    width="100%"
+                ></iframe>
             </div>
         </div>
     );
