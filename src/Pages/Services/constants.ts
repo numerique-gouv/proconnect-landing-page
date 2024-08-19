13eb905300669b63023e180b285185c847012837
rebeccadumazert <rebeccadumazert@hotmail.fr>
2024-08-19 19:39:25 +0200

    refactor: rename descriptions for services

--- src/Pages/Services/constants.ts
+++ src/Pages/Services/constants.ts
@@ -21,158 +21,164 @@ const SERVICES_LIST: Array<serviceType> = [
   {
     title: "Pad",
     url: "https://pad.numerique.gouv.fr/",
-    description: "Édition de documents textes collaborative",
+    description: "Editer des documents et des textes de manière collaborative",
     network: "internet",
   },
   {
     title: "Audioconf",
     url: "https://audioconf.numerique.gouv.fr/",
-    description: "Outil de conférence audio",
+    description: "Organiser des conférences audio",
     network: "internet",
   },
   {
     title: "Portail CISIRH RIE",
     url: "https://portail.cisirh.rie.gouv.fr/",
-    description: "Portail des applications interministérielles RH",
+    description: "Répertorier les applications interministérielles RH",
     network: "RIE",
   },
   {
     title: "Webconférence de l’État",
     url: "https://webconf.numerique.gouv.fr/",
-    description: "Outil de conférence à distance",
+    description: "Organiser des conférences à distance",
     network: "internet",
   },
   {
     title: "Osmose",
     url: "https://osmose.numerique.gouv.fr/plugins/DINUMThemePlugin/jsp/front/privateLoginOsmose.jsp?redirect=https%3A%2F%2Fosmose.numerique.gouv.fr%2Fjcms%2F",
-    description: "Outil de travail collaboratif",
+    description: "Travailler avec des outils collaboratifs",
     network: "internet",
   },
   {
     title: "Annuaire des Entreprises",
     url: "https://annuaire-entreprises.data.gouv.fr/",
     description:
-      "Vérification des informations légales publiques des entreprises, associations et services publics en France",
+      "Vérifier des informations légales publiques des entreprises, associations et services publics en France",
     network: "internet",
   },
   {
     title: "Démarches Simplifiées",
     url: "https://www.demarches-simplifiees.fr/",
-    description: "Outil pour réaliser des démarches administratives en ligne",
+    description: "Réaliser des démarches administratives en ligne",
     isOpenToPrivate: true,
     network: "internet",
   },
   {
     title: "Webinaire",
     url: "https://webinaire.numerique.gouv.fr/welcome",
-    description: "Outil de conférence à distance",
+    description: "Organiser des conférences à distance",
     network: "internet",
   },
   {
     title: "Rizomo",
     url: "https://rizomo.numerique.gouv.fr/",
     description:
-      "Portail / raccourci vers des applications collaboratives interministérielles",
+      "Accéder au portail des applications collaboratives interministérielles",
     network: "internet",
   },
   {
     title: "Socle interministériel de logiciels libres",
     url: "https://sill.code.gouv.fr/",
-    description:
-      "Catalogue de référence des logiciels libres au sein de l’État",
+    description: "Cataloguer les logiciels libres au sein de l’État",
     network: "internet",
   },
   {
     title: "France Transfert",
     url: "https://francetransfert.numerique.gouv.fr/connect",
     description:
-      "Envoi de fichiers volumineux non sensibles de manière sécurisée à un agent de l’État ou entre agent",
+      "Envoyer des fichiers volumineux non sensibles de manière sécurisée à un agent de l’État ou entre agents",
     network: "internet",
   },
   {
     title: "Mobilic",
     url: "https://mobilic.beta.gouv.fr/",
-    description:
-      "Plateforme numérique gouvernementale de suivi du temps de travail dans le transport routier léger",
+    description: "Suivre le temps de travail dans le transport routier léger",
     network: "internet",
   },
   {
     title: "Datalab SSP Cloud",
     url: "https://datalab.sspcloud.fr/home)",
     description:
-      "Plateforme mutualisée de services de traitement des données statistiques et de datascience",
+      "Répertorier les services de traitement des données statistiques et de datascience",
     network: "internet",
   },
   {
     title: "Orion du CEREMA Internet",
     url: "https://orion.cerema.fr/auth/realms/CeremaApps/account/#/",
     description:
-      "Services du CEREMA (centre d’études et d’expertise sur les risques, l’environnement, la mobilité et l’aménagement)",
+      "Répertorier les services du CEREMA (centre d’études et d’expertise sur les risques, l’environnement, la mobilité et l’aménagement)",
     network: "internet",
   },
   {
     title: "Orion du CEREMA RIE",
     url: "https://orion.cerema.e2.rie.gouv.fr/auth/realms/CeremaApps/account/#/",
     description:
-      "Services du CEREMA (centre d’études et d’expertise sur les risques, l’environnement, la mobilité et l’aménagement)",
+      "Répertorier les services du CEREMA (centre d’études et d’expertise sur les risques, l’environnement, la mobilité et l’aménagement)",
     network: "RIE",
   },
   {
     title: "Expertises Territoires",
     url: "https://www.expertises-territoires.fr/",
     description:
-      "Services du CEREMA (centre d’études et d’expertise sur les risques, l’environnement, la mobilité et l’aménagement)",
+      "Répertorier les services du CEREMA (centre d’études et d’expertise sur les risques, l’environnement, la mobilité et l’aménagement)",
     network: "internet",
   },
   {
     title: "Contrats Territoires Internet et RIE",
     url: "https://contrats-territoires.interieur.gouv.fr/accounts/login",
-    description: "Service pour élaborer les Contrats Plan État-Région (CPER)",
+    description: "Élaborer les Contrats Plan État-Région (CPER)",
     isOpenToPrivate: true,
     network: "RIE",
   },
   {
     title: "DataPass",
     url: "https://datapass.api.gouv.fr/",
-    description:
-      "Cet outil s’adresse aux ayants droits qui souhaiteraient exploiter des données à caractère personnel.",
+    description: "Régulariser l'exploitation de données à caractères personnel",
     isOpenToPrivate: true,
     network: "internet",
   },
   {
     title: "Mentor",
     url: "https://mentor.gouv.fr/login/index.php",
-    description: "Plateforme interministérielle de formation professionnelle",
+    description: "Former les professionnels",
     network: "internet",
   },
   {
     title: "VigiEau",
     url: "https://admin.vigieau.beta.gouv.fr",
+    description:
+      "S'informer sur les restrictions d'eau en période de sécheresse",
     network: "internet",
   },
   {
     title: "RIGA",
     url: "https://riga.cisirh.gouv.fr",
+    description: "Suivre la volumétrie associée aux mouvements sociaux",
     network: "internet",
   },
   {
     title: "Plus Fraîche Ma Ville",
     url: "https://plusfraichemaville.fr",
+    description:
+      "Accompagner les élus et agents pour rafraîchir durablement leur collectivité",
     network: "internet",
   },
   {
     title: "Hub d'Échange de l'État",
     url: "https://demarches.hubee.numerique.gouv.fr/",
+    description: "Recevoir des démarches administratives dématérialisées",
     network: "internet",
   },
   {
     title: "e-Campus",
     url: "https://e-campus.interieur.gouv.fr",
+    description: "",
     network: "internet",
   },
   {
     title: "SPOTE",
     url: "https://spote.developpement-durable.gouv.fr/",
+    description:
+      "Répertorier les services de l'État partenaires de la démarches naOS",
     network: "internet",
   },
 ];
