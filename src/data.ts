b589b1917844794d7201765c09939669ea6d6ad1
Benoit Serrano <benoit.serrano10@gmail.com>
2024-09-19 14:49:07 +0200

    change icon on landing page

--- src/data.ts
+++ src/data.ts
@@ -1,4 +1,4 @@
-import AgentConnect from "./assets/AgentConnect.svg";
+import HowTo from "./assets/howTo.svg";
 import EnterEmail from "./assets/enterEmail.svg";
 import Identity from "./assets/checkIdentity.svg";
 import EnjoyServices from "./assets/enjoyServices.svg";
@@ -6,7 +6,7 @@ import EnjoyServices from "./assets/enjoyServices.svg";
 export type tyleDataType = { illu: string; content: string };
 
 export const tilesData: tyleDataType[] = [
-  { illu: AgentConnect, content: "Cliquez sur le bouton Proconnect." },
+  { illu: HowTo, content: "Cliquez sur le bouton Proconnect." },
   {
     illu: EnterEmail,
     content: "Entrez votre adresse email professionnelle.",
