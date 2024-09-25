a359eb0daf34ad74793f513205b8970c07466d22
Benoit Serrano <benoit.serrano10@gmail.com>
2024-09-25 13:15:55 +0200

    add design lost page

--- src/Pages/NotFound.tsx
+++ src/Pages/NotFound.tsx
@@ -1,5 +1,27 @@
+import Button from "@codegouvfr/react-dsfr/Button";
+import ConnectionLost from "../assets/connectionLost.svg";
+
+import { useNavigate } from "react-router-dom";
+
 function NotFound() {
-  return <div>ERROR</div>;
+  let navigate = useNavigate();
+  return (
+    <div className="fr-container">
+      <div className="fr-grid-row fr-grid-row--gutters fr-grid-row--middle fr-py-5w">
+        <div className="fr-col-6">
+          <h1 className="fr-h3 fr-mb-5w">Page non trouvée</h1>
+          <p className="fr-mb-5w">
+            Nous n'arrivons pas à trouver la page que vous souhaitez afficher.
+          </p>
+
+          <Button onClick={() => navigate(-1)}>Retour</Button>
+        </div>
+        <div className="fr-col-6 centered-content">
+          <img src={ConnectionLost} alt="" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default NotFound;
