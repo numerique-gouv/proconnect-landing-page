39717618b3db41c8d06d5099743c0d465bde04ed
BenoitSerrano <benoit.serrano10@gmail.com>
2024-09-25 15:40:11 +0200

    add not found page (#31)
    * add not found page
    
    * add design lost page

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -10,87 +10,97 @@ import Stats from "../Pages/Stats";
 import EligibilityTest from "../Pages/Eligibility/EligibilityTest";
 import { PublicResult } from "../Pages/Eligibility/PublicResult";
 import { PrivateResult } from "../Pages/Eligibility/PrivateResult";
+import NotFound from "../Pages/NotFound";
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: (
+    errorElement: (
       <WrapperLayout>
-        <HomeLayout />
+        <NotFound />
       </WrapperLayout>
     ),
-  },
-  {
-    path: "/services",
-    element: (
-      <WrapperLayout>
-        <Services></Services>
-      </WrapperLayout>
-    ),
-  },
-  {
-    path: "/eligibilite",
-    element: (
-      <WrapperLayout>
-        <EligibilityTest />
-      </WrapperLayout>
-    ),
-  },
-  {
-    path: "/eligibilite/public",
-    element: (
-      <WrapperLayout>
-        <PublicResult />
-      </WrapperLayout>
-    ),
-  },
-  {
-    path: "/eligibilite/private",
-    element: (
-      <WrapperLayout>
-        <PrivateResult />
-      </WrapperLayout>
-    ),
-  },
-  {
-    path: "/feuille-de-route",
-    element: (
-      <WrapperLayout>
-        <Roadmap></Roadmap>
-      </WrapperLayout>
-    ),
-  },
-  {
-    path: "/plan-du-site",
-    element: (
-      <WrapperLayout>
-        <SiteMap />
-      </WrapperLayout>
-    ),
-  },
-  {
-    path: "/cgu",
-    element: (
-      <WrapperLayout>
-        <Cgu />
-      </WrapperLayout>
-    ),
-  },
-  {
-    path: "/mentions-legales",
-    element: (
-      <WrapperLayout>
-        <LegalNotices />
-      </WrapperLayout>
-    ),
-  },
-  {
-    path: "/stats",
-    element: (
-      <WrapperLayout>
-        <Stats />
-      </WrapperLayout>
-    ),
+    children: [
+      {
+        path: "/",
+        element: (
+          <WrapperLayout>
+            <HomeLayout />
+          </WrapperLayout>
+        ),
+      },
+      {
+        path: "/services",
+        element: (
+          <WrapperLayout>
+            <Services></Services>
+          </WrapperLayout>
+        ),
+      },
+      {
+        path: "/eligibilite",
+        element: (
+          <WrapperLayout>
+            <EligibilityTest />
+          </WrapperLayout>
+        ),
+      },
+      {
+        path: "/eligibilite/public",
+        element: (
+          <WrapperLayout>
+            <PublicResult />
+          </WrapperLayout>
+        ),
+      },
+      {
+        path: "/eligibilite/private",
+        element: (
+          <WrapperLayout>
+            <PrivateResult />
+          </WrapperLayout>
+        ),
+      },
+      {
+        path: "/feuille-de-route",
+        element: (
+          <WrapperLayout>
+            <Roadmap></Roadmap>
+          </WrapperLayout>
+        ),
+      },
+      {
+        path: "/plan-du-site",
+        element: (
+          <WrapperLayout>
+            <SiteMap />
+          </WrapperLayout>
+        ),
+      },
+      {
+        path: "/cgu",
+        element: (
+          <WrapperLayout>
+            <Cgu />
+          </WrapperLayout>
+        ),
+      },
+      {
+        path: "/mentions-legales",
+        element: (
+          <WrapperLayout>
+            <LegalNotices />
+          </WrapperLayout>
+        ),
+      },
+      {
+        path: "/stats",
+        element: (
+          <WrapperLayout>
+            <Stats />
+          </WrapperLayout>
+        ),
+      },
+    ],
   },
 ]);
 
