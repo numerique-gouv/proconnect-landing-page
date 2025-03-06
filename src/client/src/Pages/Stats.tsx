16365a5365a33d88d544874af3b1355456bcb489
Jonathan Perret <j-github@jonathanperret.net>
2025-03-06 10:45:27 +0100

    fix(stats): restore stats page with new dashboard link

--- src/client/src/Pages/Stats.tsx
+++ src/client/src/Pages/Stats.tsx
@@ -1,15 +1,18 @@
 import { useTitle } from "../lib/useTitle";
-import { CallOut } from "@codegouvfr/react-dsfr/CallOut";
 
 function Stats() {
   useTitle("Statistiques");
 
   return (
-    <div className="fr-container fr-mt-9w">
-      <CallOut iconId="ri-information-line" title="Statistiques ProConnect">
-        Les statistiques sont momentanément indisponibles. Nous nous excusons
-        pour la gêne occasionnée.
-      </CallOut>
+    <div className="fr-container">
+      <div className="fr-grid-row">
+        <iframe
+          className="fr-col-12"
+          height="1200"
+          src="https://stats.proconnect.gouv.fr/public/dashboard/8051cb4a-9e0a-49c9-81ce-3e743ba16ced"
+          width="100%"
+        ></iframe>
+      </div>
     </div>
   );
 }
