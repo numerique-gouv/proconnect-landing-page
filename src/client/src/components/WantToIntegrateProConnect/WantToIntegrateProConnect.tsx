3e888b193400f5f5b71024008bb1199fa1f9253b
Rebecca Dumazert <44778533+rebeccadumazert@users.noreply.github.com>
2025-03-24 11:31:59 +0100

    refactor(a11y): delete bad nesting of buttons and links (#98)

--- src/client/src/components/WantToIntegrateProConnect/WantToIntegrateProConnect.tsx
+++ src/client/src/components/WantToIntegrateProConnect/WantToIntegrateProConnect.tsx
@@ -1,5 +1,3 @@
-import Button from "@codegouvfr/react-dsfr/Button";
-
 function WantToIntegrateProConnect() {
   return (
     <div className="fr-container fr-col-12 fr-col-md-8 fr-py-6w fr-py-md-2w fr-mt-4w fr-mb-8w card-radius">
@@ -14,27 +12,16 @@ function WantToIntegrateProConnect() {
       </div>
       <div className="fr-container centered-text fr-my-2w">
         <a
-          className="fr-raw-link"
+          className="fr-btn fr-icon-mail-line fr-btn--icon-left fr-mr-2w fr-pl-5w fr-pr-5w"
           href="mailto:support+partenaires@proconnect.gouv.fr"
         >
-          <Button
-            className="fr-mr-2w fr-pl-5w fr-pr-5w"
-            iconId="fr-icon-mail-line"
-          >
-            Nous écrire
-          </Button>
+          Nous écrire
         </a>
         <a
-          className="fr-raw-link"
+          className="fr-btn fr-btn--secondary fr-icon-code-s-slash-line fr-btn--icon-left fr-pl-3w fr-pr-3w"
           href="https://github.com/numerique-gouv/proconnect-documentation/blob/main/README.md"
         >
-          <Button
-            priority="secondary"
-            iconId="fr-icon-code-s-slash-line"
-            className="fr-pl-3w fr-pr-3w"
-          >
-            Commencer vos tests
-          </Button>
+          Commencer vos tests
         </a>
       </div>
     </div>
