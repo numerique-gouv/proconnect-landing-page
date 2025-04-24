6aac119981d0ce2ee17e85d98c634205ff22773e
rebeccadumazert <rebeccadumazert@hotmail.fr>
2025-04-24 15:28:55 +0200

    refactor: improvment on new landing front

--- src/client/src/Pages/HomeLayout.tsx
+++ src/client/src/Pages/HomeLayout.tsx
@@ -44,38 +44,42 @@ function HomeLayout() {
           </span>
           <br />
           Certains sites peuvent être réservés à des publics spécifiques : vos
-          droits d’accès dépendent de votre adresse e-mail professionnelle et de
-          votre organisation.
+          droits d’accès dépendent de votre adresse e-mail <br />
+          professionnelle et de votre organisation.
           <br />
           Dites-nous qui est votre employeur et découvrez une sélection de sites
           faits pour vous !
           <br />
           <br />
-          <a className="fr-raw-link fr-btn" href="/recommandation-test">
+          <a
+            className="fr-btn fr-btn--lg fr-btn--icon-left fr-icon-user-heart-line"
+            href="/recommandation-test"
+          >
             Quels sites sont faits pour vous ?
           </a>
         </p>
       </div>
       <div className="fr-container fr-mb-16w">
         <div className="fr-grid-row fr-grid-row--gutters fr-col-12">
-          <div className="fr-col-12 fr-col-md-7 display-column">
+          <div className="fr-col-12 fr-col-md-6 display-column">
             <img src={profiling_transparent} width="100%" />
           </div>
-          <div className="fr-col-12 fr-col-md-5 fr-pl-md-6w">
+          <div className="fr-col-12 fr-col-md-6 fr-pl-md-4w">
             <Chip className="fr-mb-2w" text="Connexion unique" />
             <h2 className="fr-h2 fr-mb-1w">Votre identité professionnelle</h2>
             <p>
-              ProConnect vous fait entrer là où vous avez droit ! Connectez-vous
-              une fois le matin et accédez toute la journée à tous vos sites
-              partenaires autorisés, grâce à votre adresse email
-              professionnelle.
+              ProConnect vous fait entrer là où vous avez droit ! <br />
+              Connectez-vous une fois le matin et accédez toute la <br />
+              journée à tous vos sites partenaires autorisés, grâce à votre
+              <br />
+              adresse email professionnelle.
             </p>
           </div>
         </div>
       </div>
       <div className="fr-container fr-mb-16w">
         <div className="fr-grid-row fr-grid-row--gutters fr-col-12 reverse-md">
-          <div className="fr-col-12 fr-col-md-5 fr-pr-md-6w">
+          <div className="fr-col-12 fr-col-md-6 fr-pr-md-4w">
             <Chip className="fr-mb-2w" text="Diversité de partenaires" />
             <h2 className="fr-h2 fr-mb-1w">Toujours plus de partenaires</h2>
             <p>
@@ -96,14 +100,14 @@ function HomeLayout() {
               </a>
             </p>
           </div>
-          <div className="fr-col-12 fr-col-md-7 display-column">
+          <div className="fr-col-12 fr-col-md-6 display-column ">
             <img src={illu_partners} width="100%" />
           </div>
         </div>
       </div>
       <div className="fr-container fr-mb-16w">
         <div className="fr-grid-row fr-grid-row--gutters fr-col-12">
-          <div className="fr-col-12 fr-col-md-7 display-column">
+          <div className="fr-col-12 fr-col-md-6 display-column ">
             <img src={suite} width="100%" />
           </div>
 
@@ -133,16 +137,16 @@ function HomeLayout() {
       </div>
       <div className="fr-container fr-mb-16w">
         <div className="fr-grid-row fr-grid-row--gutters fr-col-12 reverse-md">
-          <div className="fr-col-12 fr-col-md-5 fr-pr-md-6w">
+          <div className="fr-col-12 fr-col-md-6 fr-pr-md-4w">
             <Chip className="fr-mb-2w" text="En cours de construction" />
             <h2 className="fr-h2 fr-mb-1w">Certification Dirigeant</h2>
             <p>
-              Pour les professionnels du secteur privé, nous vous certifions
-              bientôt en tant que dirigeant de votre entreprise pour vos
-              démarches sécurisées.
+              Pour les professionnels du secteur privé, nous vous <br />
+              certifions bientôt en tant que dirigeant de votre entreprise
+              <br /> pour vos démarches sécurisées.
             </p>
           </div>
-          <div className="fr-col-12 fr-col-md-7 display-column">
+          <div className="fr-col-12 fr-col-md-6 display-column ">
             <img src={illu_certified} width="100%" />
           </div>
         </div>
@@ -197,30 +201,30 @@ function HomeLayout() {
                 <strong>
                   Votre adresse est rattachée à une autre structure ?
                 </strong>
-                <br />
-                Créez un compte en quelques étapes simples :
-                <ol>
-                  <li>
-                    Confirmez votre adresse email à l’aide du code de validation
-                    reçu
-                  </li>
-                  <li>Choisissez un mot de passe</li>
-                  <li>
-                    Entrez le SIRET de votre organisation (disponible sur{" "}
-                    <a
-                      className="fr-link"
-                      target="_blank"
-                      rel="noopener external"
-                      href="https://annuaire-entreprises.data.gouv.fr/"
-                    >
-                      l'Annuaire des Entreprises
-                    </a>
-                    )
-                  </li>
-                  <li>Complétez vos informations personnelles</li>
-                  <li>Votre compte est créé !</li>
-                </ol>
               </p>
+              <br />
+              Créez un compte en quelques étapes simples :
+              <ol>
+                <li>
+                  Confirmez votre adresse email à l’aide du code de validation
+                  reçu
+                </li>
+                <li>Choisissez un mot de passe</li>
+                <li>
+                  Entrez le SIRET de votre organisation (disponible sur{" "}
+                  <a
+                    className="fr-link"
+                    target="_blank"
+                    rel="noopener external noreferrer"
+                    href="https://annuaire-entreprises.data.gouv.fr/"
+                  >
+                    l'Annuaire des Entreprises
+                  </a>
+                  )
+                </li>
+                <li>Complétez vos informations personnelles</li>
+                <li>Votre compte est créé !</li>
+              </ol>
             </div>
           </section>
           <section className="fr-accordion">
@@ -236,51 +240,51 @@ function HomeLayout() {
             <div className="fr-collapse" id="accordion-116">
               <p>
                 Vous êtes peut-être dans l’une de ces situations :
-                <ul>
-                  <li>
+              </p>
+              <ul>
+                <li>
                     Vous avez fait une erreur de saisie dans votre adresse mail
                     : recréez un compte avec la bonne adresse
-                  </li>
-                  <li>
+                </li>
+                <li>
                     Le code est arrivé dans vos courriers indésirables :
                     vérifiez vos spams
-                  </li>
-                  <li>
-                    Votre organisation utilise une protection contre les spams
+                </li>
+                <li>
+                  Votre organisation utilise une protection contre les spams
                     (comme MailInBlack ou Altospam) : contactez votre
                     fournisseur de mail pour qu’il autorise les emails en
                     provenance de nepasrepondre@email.moncomptepro.beta.gouv.fr
                     (adresse IP : 172.246.41.163). Puis, demandez-lui de
                     paramétrer le filtre anti-spam pour laisser passer nos
                     emails.
-                  </li>
-                  <li>
-                    Votre code a expiré ou vous avez perdu l’email qui contenait
-                    le code : vous pouvez vous reconnecter en cliquant sur « me
+                </li>
+                <li>
+                  Votre code a expiré ou vous avez perdu l’email qui contenait
+                  le code : vous pouvez vous reconnecter en cliquant sur « me
                     renvoyer un code de confirmation » lorsqu'il vous sera
                     demandé de saisir le code (
-                    <a
-                      className="fr-link"
-                      target="_blank"
-                      rel="noopener external"
-                      href="https://proconnect.crisp.help/fr/article/comment-regler-les-problematiques-liees-au-code-de-confirmation-a-10-chiffres-jyepuz/?bust=1744729438909"
-                    >
-                      en savoir plus
-                    </a>
-                    )
-                  </li>
-                </ul>
-              </p>
+                  <a
+                    className="fr-link"
+                    target="_blank"
+                    rel="noopener external noreferrer"
+                    href="https://proconnect.crisp.help/fr/article/comment-regler-les-problematiques-liees-au-code-de-confirmation-a-10-chiffres-jyepuz/?bust=1744729438909"
+                  >
+                    en savoir plus
+                  </a>
+                  )
+                </li>
+              </ul>
             </div>
           </section>
         </div>
       </div>
-      <div className="fr-container fr-mb-10w">
+      <div className="fr-container fr-mt-5w fr-mb-10w">
         <p>
           <a
             className="fr-link"
             target="_blank"
-            rel="noopener external"
+            rel="noopener external noreferrer"
             href="https://proconnect.crisp.help/fr/"
           >
             Voir tous les articles du centre d'aide
