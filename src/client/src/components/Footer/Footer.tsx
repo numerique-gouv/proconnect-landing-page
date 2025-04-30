1f7dec48b01a386f605b21b2e8f8e6ee2ac39164
rebeccadumazert <rebeccadumazert@hotmail.fr>
2025-04-30 12:02:50 +0200

    a11y(links): Add target="_blank", title, rel="noreferrer noopener", keys on links

--- src/client/src/components/Footer/Footer.tsx
+++ src/client/src/components/Footer/Footer.tsx
@@ -13,21 +13,29 @@ function Footer() {
         to: "/plan-du-site",
       }}
       bottomItems={[
-        <a href="/cgu" className="fr-footer__bottom-link">
+        <a key="cgu" href="/cgu" className="fr-footer__bottom-link">
           Conditions générales d'utilisation
         </a>,
-        <a href="/stats" className="fr-footer__bottom-link">
+        <a key="statistiques" href="/stats" className="fr-footer__bottom-link">
           Statistiques d'utilisation
         </a>,
         <a
+          key="statut"
           href="https://status.proconnect.gouv.fr/"
           className="fr-footer__bottom-link"
+          target="_blank"
+          rel="noreferrer noopener"
+          title="Statuts des services - nouvelle fenêtre"
         >
           Statuts des services
         </a>,
         <a
+          key="partenaire"
           href="https://partenaires.proconnect.gouv.fr/"
           className="fr-footer__bottom-link"
+          target="_blank"
+          rel="noreferrer noopener"
+          title="Devenez un FI ou FS partenaire - nouvelle fenêtre"
         >
           Devenez un FI ou FS partenaire
         </a>,
