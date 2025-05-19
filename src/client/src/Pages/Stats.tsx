904c652a4c5a40b38a577b00a962edaa5ecef355
rebeccadumazert <rebeccadumazert@hotmail.fr>
2025-05-19 18:24:22 +0200

    feat(stats): updatemetabase  iframe src to use dynamic theme based on dark mode

--- src/client/src/Pages/Stats.tsx
+++ src/client/src/Pages/Stats.tsx
@@ -1,15 +1,19 @@
 import { useTitle } from "../lib/useTitle";
+import { useIsDark } from "@codegouvfr/react-dsfr/useIsDark";
 
 function Stats() {
   useTitle("Statistiques");
+  const { isDark } = useIsDark();
 
+  const metabaseTheme = isDark ? "#theme=night" : "";
+  const url = `https://stats.proconnect.gouv.fr/public/dashboard/8051cb4a-9e0a-49c9-81ce-3e743ba16ced${metabaseTheme}`;
   return (
     <div className="fr-container">
       <div className="fr-grid-row">
         <iframe
           className="fr-col-12"
           height="1200"
-          src="https://stats.proconnect.gouv.fr/public/dashboard/8051cb4a-9e0a-49c9-81ce-3e743ba16ced"
+          src={url}
           width="100%"
         ></iframe>
       </div>
